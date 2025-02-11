3468b0ebcc3ce506204f696da1ff188e4651e97a
Kiaryy <martin360.moron@gmail.com>
2025-02-11 14:05:29 -0300

    Added main menu, textures for main menu and gameplay backgrounds

--- main.cpp
+++ main.cpp
@@ -3,30 +3,54 @@
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
+typedef enum GameScreen { TITLE = 0, GAMEPLAY, ENDING } GameScreen;
+
 int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
-    
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
     
+    Image image = LoadImage("assets/title_background.png");     // Loaded in CPU memory (RAM)
+    Texture2D title_background = LoadTextureFromImage(image);          // Image converted to texture, GPU memory (VRAM)
+    UnloadImage(image);   // Once image has been converted to texture and uploaded to VRAM, it can be unloaded from RAM
+    image = LoadImage("assets/gameplay_background.png");
+    Texture2D gameplay_background = LoadTextureFromImage(image);
+    UnloadImage(image);
+
+    GameScreen currentScreen = TITLE;
+
+
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
     
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        HideCursor();
-
         BeginDrawing();
-            ClearBackground(RAYWHITE);
-            // im sure there's a better way to center this but this is what i could find in the cheatsheet
-            DrawText(TextFormat("Ball position is:\nX:%i Y:%i", GetMouseX(), GetMouseY()), screenWidth/2 - MeasureText(TextFormat("Ball position is:\nX:%i Y:%i", GetMouseX(), GetMouseY()),20)/2, screenHeight/2, 20, LIGHTGRAY);
-            DrawCircle(GetMouseX(), GetMouseY(),15, {255,0,0,255});
+            HideCursor();
+            // Switch to handle different screens
+            switch (currentScreen) {
+                case TITLE:
+                    DrawTexture(title_background, 0, 0, WHITE);
+                    DrawText("Ball Shooter", 20, 20, 40, BLACK);
+                    DrawText("PRESS ENTER TO START", 120, 220, 20, BLACK);
+                    if (IsKeyPressed(KEY_ENTER)){
+                        currentScreen = GAMEPLAY;
+                    }
+                break;
+                case GAMEPLAY:
+                    DrawTexture(gameplay_background, 0, 0, WHITE);
+                    // im sure there's a better way to center this but this is what i could find in the cheatsheet
+                    DrawText(TextFormat("Ball position is:\nX:%i Y:%i", GetMouseX(), GetMouseY()), screenWidth/2 - MeasureText(TextFormat("Ball position is:\nX:%i Y:%i", GetMouseX(), GetMouseY()),20)/2, screenHeight/2, 20, BLACK);
+                    DrawCircle(GetMouseX(), GetMouseY(),15, {255,0,0,255});
+                break;
+                case ENDING:
+                    //TODO Add Game Over screen
+                break;                
+            }
 
         EndDrawing();
         //----------------------------------------------------------------------------------
