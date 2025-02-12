a33351bfb0b2ddd3dd05b1a9a82f1caa16d1772d
Kiaryy <martin360.moron@gmail.com>
2025-02-12 19:26:05 -0300

    Added title screen music and game music place holder

--- main.cpp
+++ main.cpp
@@ -11,7 +11,12 @@ int main(void)
     //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "Ball Shooter");
+    
+    InitAudioDevice();
+    Music title_music = LoadMusicStream("assets/title.mp3");
+    Music game_music = LoadMusicStream("assets/place_holder.mp3");
+    PlayMusicStream(title_music);
     
     Image image = LoadImage("assets/title_background.png");     // Loaded in CPU memory (RAM)
     Texture2D title_background = LoadTextureFromImage(image);          // Image converted to texture, GPU memory (VRAM)
@@ -27,8 +32,9 @@ int main(void)
     //--------------------------------------------------------------------------------------
     
     // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
+    while (!WindowShouldClose()){   // Detect window close button or ESC key    
+        UpdateMusicStream(title_music); 
+        UpdateMusicStream(game_music);
         BeginDrawing();
             HideCursor();
             // Switch to handle different screens
@@ -38,10 +44,15 @@ int main(void)
                     DrawText("Ball Shooter", 20, 20, 40, BLACK);
                     DrawText("PRESS ENTER TO START", 120, 220, 20, BLACK);
                     if (IsKeyPressed(KEY_ENTER)){
+                        StopMusicStream(title_music);
                         currentScreen = GAMEPLAY;
                     }
                 break;
                 case GAMEPLAY:
+                    if (!IsMusicStreamPlaying(game_music)) {
+                        PlayMusicStream(game_music);
+                    }    
+                    UpdateMusicStream(game_music);
                     DrawTexture(gameplay_background, 0, 0, WHITE);
                     // im sure there's a better way to center this but this is what i could find in the cheatsheet
                     DrawText(TextFormat("Ball position is:\nX:%i Y:%i", GetMouseX(), GetMouseY()), screenWidth/2 - MeasureText(TextFormat("Ball position is:\nX:%i Y:%i", GetMouseX(), GetMouseY()),20)/2, screenHeight/2, 20, BLACK);
@@ -55,7 +66,8 @@ int main(void)
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
-
+    UnloadMusicStream(title_music);
+    CloseAudioDevice();
     // De-Initialization
     //--------------------------------------------------------------------------------------
     CloseWindow();        // Close window and OpenGL context
