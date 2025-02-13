b7ee75496c3641dae4a28336ed7c6e28681a8804
Kiaryy <martin360.moron@gmail.com>
2025-02-13 15:39:23 -0300

    Added crosshair, tilts towards the direction its moving

--- main.cpp
+++ main.cpp
@@ -5,6 +5,17 @@
 //------------------------------------------------------------------------------------
 typedef enum GameScreen { TITLE = 0, GAMEPLAY, ENDING } GameScreen;
 
+float clamp(float value, float min, float max){
+    if(value < min){
+        return min;
+    }
+    if (value > max){
+        return max;
+    }
+    return value;
+}
+
+
 int main(void)
 {
     // Initialization
@@ -41,23 +52,35 @@ int main(void)
     Texture cat_07 = LoadTextureFromImage(image);
     image = LoadImage("assets/cat_08.png");
     Texture cat_08 = LoadTextureFromImage(image);
+    image = LoadImage("assets/crosshair.png");
+    Texture crosshair = LoadTextureFromImage(image);
     UnloadImage(image);
 
+    // Explosion gif
+    // https://www.raylib.com/examples/textures/loader.html?name=textures_gif_player
+    int animFrames = 0;
+    Image imScarfyAnim = LoadImageAnim("assets/explosion.gif", &animFrames);
+    Texture2D texScarfyAnim = LoadTextureFromImage(imScarfyAnim);
+    
+    unsigned int nextFrameDataOffset = 0;
+    int currentAnimFrame = 0;       
+    int frameDelay = 8;             
+    int frameCounter = 0;  
+
     GameScreen currentScreen = TITLE;
-
-
+    float crossHairRotation = 0.0f;
+    int previousMouseX = GetMouseX();
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
     
     // Main game loop
     while (!WindowShouldClose()){   // Detect window close button or ESC key    
-        UpdateMusicStream(title_music); 
-        UpdateMusicStream(game_music);
         BeginDrawing();
-            HideCursor();
-            // Switch to handle different screens
+        HideCursor();
+        // Switch to handle different screens
             switch (currentScreen) {
                 case TITLE:
+                    UpdateMusicStream(title_music); 
                     DrawTexture(title_background, 0, 0, WHITE);
                     DrawText("Cat Shooter", 20, 20, 40, BLACK);
                     DrawText("PRESS ENTER TO START", 120, 220, 20, BLACK);
@@ -74,7 +97,7 @@ int main(void)
                     DrawTexture(gameplay_background, 0, 0, WHITE);
                     // im sure there's a better way to center this but this is what i could find in the cheatsheet
                     DrawText(TextFormat("Cursor position is:\nX:%i Y:%i", GetMouseX(), GetMouseY()), screenWidth/2 - MeasureText(TextFormat("Ball position is:\nX:%i Y:%i", GetMouseX(), GetMouseY()),20)/2, screenHeight/2, 20, BLACK);
-                    // DrawCircle(GetMouseX(), GetMouseY(),15, {255,0,0,255});
+                    
                     DrawTextureEx(cat_01, {0, 0}, 0, 0.5, WHITE);
                     DrawTextureEx(cat_02, {100, 0}, 0, 0.5, WHITE);
                     DrawTextureEx(cat_03, {200, 0}, 0, 0.5, WHITE);
@@ -84,7 +107,40 @@ int main(void)
                     DrawTextureEx(cat_07, {600, 0}, 0, 0.5, WHITE);
                     DrawTextureEx(cat_08, {700, 0}, 0, 0.5, WHITE);
 
+                    
+                    frameCounter++;
+                    if (frameCounter >= frameDelay){
+                        // Move to next frame
+                        // NOTE: If final frame is reached we return to first frame
+                        currentAnimFrame++;
+                        if (currentAnimFrame >= animFrames) currentAnimFrame = 0;
+                        // Get memory offset position for next frame data in image.data
+                        nextFrameDataOffset = imScarfyAnim.width*imScarfyAnim.height*4*currentAnimFrame;
+                        // Update GPU texture data with next frame image data
+                        // WARNING: Data size (frame size) and pixel format must match already created texture
+                        UpdateTexture(texScarfyAnim, ((unsigned char *)imScarfyAnim.data) + nextFrameDataOffset);
 
+                        frameCounter = 0;
+                    }
+                    DrawTextureEx(texScarfyAnim, {0, 50}, 0, 0.25, WHITE);
+
+
+
+                    // Determine if crosshair is going to the left or to the right
+                    if (previousMouseX < GetMouseX()) {
+                        crossHairRotation += 2.0f;
+                    } else if (previousMouseX > GetMouseX()) {
+                        crossHairRotation -= 2.0f;  
+                    } else {
+                        crossHairRotation = 0.0f;
+                    }
+
+                    DrawTexturePro(crosshair, {0,0, (float)crosshair.width, (float)crosshair.height},
+                    {(float)GetMouseX(), (float)GetMouseY(), (float)crosshair.width/4, (float)crosshair.height/4},
+                    {(float)crosshair.width/8, (float)crosshair.height/8},
+                    clamp(crossHairRotation, -25, 25),
+                    WHITE);
+                    previousMouseX = GetMouseX(); // We update the previous position
                 break;
                 case ENDING:
                     //TODO Add Game Over screen
