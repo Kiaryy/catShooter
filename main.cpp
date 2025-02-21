e56149c12eb83c1f65e13c7bb0c966a26f38641e
Kiaryy <martin360.moron@gmail.com>
2025-02-21 14:56:20 -0300

    Added game over, music file not included because its not original

--- main.cpp
+++ main.cpp
@@ -17,13 +17,15 @@ int main(void)
     InitAudioDevice();
     Music title_music = LoadMusicStream("assets/title.mp3");
     Music game_music = LoadMusicStream("assets/place_holder.mp3");
-    PlayMusicStream(title_music);
-        
+    Music gameover_music = LoadMusicStream("assets/gameover.mp3");
+    SetMusicVolume(gameover_music, 0.5f);
     // Backgrounds
     Image image = LoadImage("assets/title_background.png");     // Loaded in CPU memory (RAM)
     Texture2D title_background = LoadTextureFromImage(image);          // Image converted to texture, GPU memory (VRAM)
     image = LoadImage("assets/gameplay_background.png");
     Texture2D gameplay_background = LoadTextureFromImage(image);
+    image = LoadImage("assets/gameover_background.png");
+    Texture2D gameover_background = LoadTextureFromImage(image);
     UnloadImage(image);
 
     // Cats!!
@@ -32,7 +34,8 @@ int main(void)
     // crosshair
     Crosshair crosshair{};
 
-    int score = 0;
+    int score;
+    int health;
 
     GameScreen currentScreen = TITLE;
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
@@ -46,7 +49,15 @@ int main(void)
         // Switch to handle different screens
             switch (currentScreen) {
                 case TITLE:
+                    // Music stuff
+                    if (!IsMusicStreamPlaying(title_music)) {
+                        PlayMusicStream(title_music);
+                    }   
                     UpdateMusicStream(title_music); 
+                    // We set (or reset) game data
+                    health = 3;
+                    score = 0;
+
                     DrawTexture(title_background, 0, 0, WHITE);
                     DrawText("Cat Shooter", 20, 20, 40, BLACK);
                     DrawText("PRESS ENTER TO START", 120, 220, 20, BLACK);
@@ -64,20 +75,35 @@ int main(void)
                     DrawTexture(gameplay_background, 0, 0, WHITE);
                     // im sure there's a better way to center this but this is what i could find in the cheatsheet
                     DrawText(TextFormat("Cursor position is:\nX:%i Y:%i", GetMouseX(), GetMouseY()), screenWidth/2 - MeasureText(TextFormat("Ball position is:\nX:%i Y:%i", GetMouseX(), GetMouseY()),20)/2, screenHeight/2, 20, BLACK);
-                    DrawText(TextFormat("Score: %i", score), 680 - MeasureText(TextFormat("Ball position is:\nX:%i Y:%i", GetMouseX(), GetMouseY()),20)/2, 25, 20, BLACK);
+                    DrawText(TextFormat("Score: %i", score), 700 - MeasureText(TextFormat("Ball position is:\nX:%i Y:%i", GetMouseX(), GetMouseY()),20)/2, 25, 20, BLACK);
+                    DrawText(TextFormat("Health: %i", health), 100 - MeasureText(TextFormat("Ball position is:\nX:%i Y:%i", GetMouseX(), GetMouseY()),20)/2, 25, 20, BLACK);
                     
-                    
-                    cat1.draw(deltaTime, score);
-                    cat2.draw(deltaTime, score);
+                    cat1.draw(deltaTime, score, health);
                     cat1.checkForClick(GetMousePosition());
+                    cat1.move();
+                    cat2.draw(deltaTime, score, health);
                     cat2.checkForClick(GetMousePosition());
-
+                    cat2.move();
+                    
                     
                     crosshair.drawCrosshair();
 
+                    if(health <= 0){
+                        currentScreen = ENDING;
+                    }
                 break;
                 case ENDING:
-                    //TODO Add Game Over screen
+                    if (!IsMusicStreamPlaying(gameover_music)) {
+                        PlayMusicStream(gameover_music);
+                    }    
+                    UpdateMusicStream(gameover_music);
+                    DrawTexture(gameover_background, 0, 0, WHITE);
+                    DrawText("Game Over!", 300, 150, 40, BLACK);
+                    DrawText("PRESS ENTER TO TRY AGAIN", 250, 220, 20, BLACK);
+                    if (IsKeyPressed(KEY_ENTER)){
+                        StopMusicStream(title_music);
+                        currentScreen = TITLE;
+                    }
                 break;                
             }
 
--- readme.md
+++ readme.md
@@ -1,13 +1,27 @@
-# Cat shooter 
+# Cat shooter
+
 Cat shooter is a small game made with c++ and Raylib.
+
 The objective of the game is to shoot as many cats as you can!
+
 ## How to install
+
 Compile using clang with the provided makefile, or download the binary from the releases page (if i feel like doing that)
 
+  
+
 ### TODO
-1. Code basic gameplay
-2. Background music 
-3. Code game over
+
+1. <del>Code basic gameplay
+
+2. Background music
+
+3. <del>Code game over
+
 4. Add cats!
+
+5. Add more effects
+
+  
 
 ![hippo](https://media.tenor.com/gIujj5bPWmMAAAAi/maxwell.gif)
